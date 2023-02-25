8694461b5c1307ce7a79637b07c076771fbca3a0
MileyJump <choialsrud2002@gmail.com>
2023-02-25 13:02:18 +0900

    Initial Commit

---
+++ Wanted-Pre-onbording-Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wanted-Pre-onbording-Challenge
+//
+//  Created by 최민경 on 2023/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
