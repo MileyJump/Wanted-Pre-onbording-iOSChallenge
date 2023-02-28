103f879fde35fc40aacfb3ac010124db9ca9dd0b
MileyJump <choialsrud2002@gmail.com>
2023-02-28 22:23:29 +0900

    1. URLSession 2. UIButton 클릭 URLSession 함수 호출

--- Wanted-Pre-onbording-Challenge/ViewController.swift
+++ Wanted-Pre-onbording-Challenge/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var selectimageIndex = 0
+    
     
     // 구글드라이브에 업로드한 이미지 다운로드 URL
     
@@ -36,30 +36,77 @@ class ViewController: UIViewController {
         uiImageView5.image = #imageLiteral(resourceName: "puppy-g0af19d0a0_1280 (1)")
     }
     
+    
     @IBAction func button1(_ sender: Any) {
+        imageindex = 0
         uiImageView1.image = UIImage(systemName: "photo.fill")
-        
+        imageDownload(imageView: uiImageView1)
     
     }
+
+    @IBAction func button2(_ sender: Any) {
+        imageindex = 1
+        uiImageView2.image = UIImage(systemName: "photo.fill")
+        imageDownload(imageView: uiImageView2)
+    }
+    
+    @IBAction func button3(_ sender: Any) {
+        imageindex = 2
+        uiImageView3.image = UIImage(systemName: "photo.fill")
+        imageDownload(imageView: uiImageView3)
+    }
+    
+    @IBAction func button4(_ sender: Any) {
+        imageindex = 3
+        uiImageView4.image = UIImage(systemName: "photo.fill")
+        imageDownload(imageView: uiImageView4)
+    }
+    
+    @IBAction func button5(_ sender: Any) {
+        imageindex = 4
+        uiImageView5.image = UIImage(systemName: "photo.fill")
+        imageDownload(imageView: uiImageView5)
+    }
+    
+    
+    @IBAction func allLoadImagesbutton(_ sender: Any) {
+        imageindex = 0
+        uiImageView1.image = UIImage(systemName: "photo.fill")
+        imageDownload(imageView: uiImageView1)
         
-        // 2. 다운로드 되야함
+        imageindex = 1
+        uiImageView2.image = UIImage(systemName: "photo.fill")
+        imageDownload(imageView: uiImageView2)
         
+        imageindex = 2
+        uiImageView3.image = UIImage(systemName: "photo.fill")
+        imageDownload(imageView: uiImageView3)
+        
+        imageindex = 3
+        uiImageView4.image = UIImage(systemName: "photo.fill")
+        imageDownload(imageView: uiImageView4)
+        
+        imageindex = 4
+        uiImageView5.image = UIImage(systemName: "photo.fill")
+        imageDownload(imageView: uiImageView5)
+    }
+    
+    
     }
 
 
 
+var imageindex = 0
     
     
     
-    
-    
-let googleDriveImageURL =
-    "https://drive.google.com/uc?export=download&id=11B5f5exhaFYtwoBNNua_3iB5KB1m5sdm"
-//"https://drive.google.com/uc?export=download&id=1nmmwk-cYOchEKcQa8RmCI3f-dG-5hC2Z",
-//"https://drive.google.com/uc?export=download&id=178EPAFe23m3nbDaOTPEuQ-gCTmt5CiHX",
-//    "https://drive.google.com/uc?export=download&id=1A4ZbTt0373q93O4915GsHMowqv8hrM6u",
-//    "https://drive.google.com/uc?export=download&id=1ifGI2cmLhTEMWdBZvW_ns9LDe9wHUbA7"
-//]
+let googleDriveImageURL = [
+    "https://drive.google.com/uc?export=download&id=11B5f5exhaFYtwoBNNua_3iB5KB1m5sdm",
+"https://drive.google.com/uc?export=download&id=1nmmwk-cYOchEKcQa8RmCI3f-dG-5hC2Z",
+"https://drive.google.com/uc?export=download&id=178EPAFe23m3nbDaOTPEuQ-gCTmt5CiHX",
+    "https://drive.google.com/uc?export=download&id=1A4ZbTt0373q93O4915GsHMowqv8hrM6u",
+    "https://drive.google.com/uc?export=download&id=1ifGI2cmLhTEMWdBZvW_ns9LDe9wHUbA7"
+]
 
     
     
@@ -68,7 +115,7 @@ let googleDriveImageURL =
         
         
         
-        let strcutURL = URL(string: googleDriveImageURL)!
+        let strcutURL = URL(string: googleDriveImageURL[imageindex])!
         
         URLSession.shared.dataTask(with: strcutURL) { data, response, error in
             
@@ -78,11 +125,12 @@ let googleDriveImageURL =
                 return
             }
             
-            if let data = data {
-              print("데이터를 받아오지 못 했습니다" + String(decoding: data, as: UTF8.self))
+            guard let savedata = data else{
+                print("데이터를 받아오지 못 했습니다")
+                return
             }
             
-            let image = UIImage(data: data)
+            let image = UIImage(data: savedata)
             
             DispatchQueue.main.async() {
                 print("이미지 다운로드")
